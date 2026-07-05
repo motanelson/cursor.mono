b30bfc6d0b1022c177411971e9d1bcec54b02da9
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-05 05:12:26 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("\033c\033[47;31m\nhello world!");
+    }
+}
--- jbat.py
+++ jbat.py
@@ -1,10 +1,11 @@
 import os
 import copy
 compilers="javac --release 8 "
-decompilers="javap -c -private "
+decompilers="openjdk-asmtools-jdis $1.class -w . "
 print("\033c\033[47;31m\ngive me file to jbat: ? \n")
-a=input().strip()
 #a="Hello.java"
+a=input().strip()
 b=a.replace(".java","")
 os.system(compilers + a)
-os.system(decompilers + b+".class > "+b +".jbat")
+os.system(decompilers.replace("$1",b)  )
+os.rename("$1.jasm".replace("$1",b),"$1.jbat".replace("$1",b))
